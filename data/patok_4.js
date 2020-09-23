9a07dcc33d6323e90c6e69ad9d3ee9a27b370db5
ardiansyahgeo <67992886+ardiansyahgeo@users.noreply.github.com>
2020-09-23 09:39:53 +0700

    Add files via upload

---
+++ data/patok_4.js
@@ -0,0 +1,23 @@
+var json_patok_4 = {
+"type": "FeatureCollection",
+"name": "patok_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "1", "Name": "A0", "X": 106.90142427799999, "Y": -6.369066753, "Z": 100.0, "Segment": "Segment_1" }, "geometry": { "type": "Point", "coordinates": [ 106.901424278000036, -6.36906675299997 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "2", "Name": "A1", "X": 106.902418402, "Y": -6.368185536, "Z": 99.495, "Segment": "Segment_1" }, "geometry": { "type": "Point", "coordinates": [ 106.90241840200008, -6.368185535999942 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "3", "Name": "A2", "X": 106.902937708, "Y": -6.367794906, "Z": 100.061, "Segment": "Segment_2" }, "geometry": { "type": "Point", "coordinates": [ 106.902937708000081, -6.367794905999972 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "4", "Name": "A3", "X": 106.902990851, "Y": -6.367624665, "Z": 99.805, "Segment": "Segment_2" }, "geometry": { "type": "Point", "coordinates": [ 106.902990851000084, -6.367624664999937 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "5", "Name": "A4", "X": 106.902383818, "Y": -6.367979719, "Z": 98.905, "Segment": "Segment_2" }, "geometry": { "type": "Point", "coordinates": [ 106.902383818000033, -6.367979718999948 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "6", "Name": "A5", "X": 106.902488695, "Y": -6.367589492, "Z": 96.28, "Segment": "Segment_2" }, "geometry": { "type": "Point", "coordinates": [ 106.902488695000045, -6.367589491999979 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "7", "Name": "A6", "X": 106.90261106299999, "Y": -6.367301374, "Z": 94.81, "Segment": "Segment_2" }, "geometry": { "type": "Point", "coordinates": [ 106.902611063000052, -6.367301373999965 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "8", "Name": "A7", "X": 106.90237327299999, "Y": -6.367376284, "Z": 94.087, "Segment": "Segment_2" }, "geometry": { "type": "Point", "coordinates": [ 106.90237327300008, -6.367376283999931 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "9", "Name": "A8", "X": 106.902038444, "Y": -6.367606943, "Z": 94.635, "Segment": "Segment_3" }, "geometry": { "type": "Point", "coordinates": [ 106.902038444000084, -6.367606942999942 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "10", "Name": "A9", "X": 106.90157591099999, "Y": -6.36799626, "Z": 96.075, "Segment": "Segment_3" }, "geometry": { "type": "Point", "coordinates": [ 106.901575911000066, -6.367996259999927 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "11", "Name": "A10", "X": 106.901212079, "Y": -6.368341863, "Z": 96.47, "Segment": "Segment_3" }, "geometry": { "type": "Point", "coordinates": [ 106.901212079000061, -6.368341862999955 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "12", "Name": "A11", "X": 106.90102228799999, "Y": -6.368437631, "Z": 96.59, "Segment": "Segment_3" }, "geometry": { "type": "Point", "coordinates": [ 106.901022288000036, -6.368437630999949 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "13", "Name": "A12", "X": 106.90090895199999, "Y": -6.368523743, "Z": 96.508, "Segment": "Segment_3" }, "geometry": { "type": "Point", "coordinates": [ 106.900908952000066, -6.368523742999969 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "14", "Name": "A13", "X": 106.90077973699999, "Y": -6.368646918, "Z": 96.263, "Segment": "Segment_3" }, "geometry": { "type": "Point", "coordinates": [ 106.90077973700005, -6.368646917999968 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "15", "Name": "A14", "X": 106.900664037, "Y": -6.368897823, "Z": 96.298, "Segment": "Segment_3" }, "geometry": { "type": "Point", "coordinates": [ 106.90066403700007, -6.368897822999941 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "16", "Name": "A15", "X": 106.900572947, "Y": -6.36906003, "Z": 96.218, "Segment": "Segment_3" }, "geometry": { "type": "Point", "coordinates": [ 106.900572947000057, -6.369060029999957 ] } }
+]
+}
